1a84b0eb827466a7b809a3e362ed91841b02ca13
gzamfir-ca <gzamfir_ca@icloud.com>
2025-10-25 13:57:34 -0400

    +system cmd

--- lib/development.rb
+++ lib/development.rb
@@ -3,16 +3,22 @@
 require_relative "development/pod"
 require_relative "development/pod/version"
 
-def reload!(print: true)
-  puts "Reloading..." if print
+def reload!(print: false)
   root_dir = File.expand_path("..", __dir__ || Dir.pwd)
   reload_dirs = %w[lib]
   reload_dirs.each do |dir|
     Dir["#{root_dir}/#{dir}/**/*.rb"].each do |file|
+      next if file.include?("version")
+
+      puts "reloading: #{file}" if print
       load file
     end
   end
   true
+end
+
+def system!(*args)
+  system("/bin/bash", "-c", *args) || raise("command failed: #{args.join(" ")}")
 end
 
 # provides development-related utilities
