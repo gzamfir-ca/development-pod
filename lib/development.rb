3e170c8f9800bf2198b534ebd9757ec0300cd1c1
gzamfir-ca <gzamfir_ca@icloud.com>
2025-11-01 21:49:55 -0400

    major cleanup

--- lib/development.rb
+++ lib/development.rb
@@ -3,9 +3,21 @@
 require_relative "development/cli"
 require_relative "development/pod"
 require_relative "development/pod/version"
+require "shellwords"
+
+# define helper methods
+def load_file(file, print)
+  load file
+rescue LoadError => e
+  puts "reloading failed: #{e.message}" if print
+rescue SyntaxError => e
+  puts "reloading failed: #{e.message}" if print
+rescue StandardError => e
+  puts "reloading failed: #{e.message}" if print
+end
 
 # define system-wide utilities
-def reload!(print: false)
+def reload!(print: true)
   root_dir = File.expand_path("..", __dir__ || Dir.pwd)
   reload_dirs = %w[lib]
   reload_dirs.each do |dir|
@@ -13,14 +25,19 @@ def reload!(print: false)
       next if file.include?("version")
 
       puts "reloading: #{file}" if print
-      load file
+      load_file(file, print)
     end
   end
   true
 end
 
-def system!(*args)
-  system("/bin/bash", "-c", *args) || raise("command failed: #{args.join(" ")}")
+def system!(*args, print: true)
+  command = Shellwords.join(args)
+  system("/bin/bash", "-c", command, exception: true)
+rescue RuntimeError => e
+  puts "command run failed: #{e.message}" if print
+rescue SystemCallError => e
+  puts "system call failed: #{e.message}" if print
 end
 
 # provides development-related utilities
