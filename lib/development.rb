94343436a103477afa5e5e6e46ed21dc8a864d33
gzamfir-ca <gzamfir_ca@icloud.com>
2025-11-05 12:34:01 -0500

    +upload cmd

--- lib/development.rb
+++ lib/development.rb
@@ -4,16 +4,43 @@ require_relative "development/cli"
 require_relative "development/pod"
 require_relative "development/pod/version"
 require "shellwords"
+require "yaml"
 
 # define helper methods
 def load_file(file, print)
   load file
 rescue LoadError => e
-  puts "reloading failed: #{e.message}" if print
+  puts "loading file failed: #{e.message}" if print
+  nil
 rescue SyntaxError => e
-  puts "reloading failed: #{e.message}" if print
+  puts "loading file failed: #{e.message}" if print
+  nil
 rescue StandardError => e
-  puts "reloading failed: #{e.message}" if print
+  puts "loading file failed: #{e.message}" if print
+  nil
+end
+
+def load_yaml(file, print)
+  YAML.safe_load_file(file, permitted_classes: [Time])
+rescue Psych::SyntaxError => e
+  puts "loading yaml failed: #{e.message}" if print
+  nil
+rescue Psych::DisallowedClass => e
+  puts "loading yaml failed: #{e.message}" if print
+  nil
+rescue StandardError => e
+  puts "loading yaml failed: #{e.message}" if print
+  nil
+end
+
+def shell_cmd(cmd, print)
+  system("/bin/bash", "-c", cmd, exception: true)
+rescue RuntimeError => e
+  puts "command run failed: #{e.message}" if print
+  nil
+rescue SystemCallError => e
+  puts "system call failed: #{e.message}" if print
+  nil
 end
 
 # define system-wide utilities
@@ -34,11 +61,12 @@ end
 def system!(*args, print: true)
   cmd = Shellwords.join(args)
   puts "executing: #{cmd}" if print
-  system("/bin/bash", "-c", cmd, exception: true)
-rescue RuntimeError => e
-  puts "command run failed: #{e.message}" if print
-rescue SystemCallError => e
-  puts "system call failed: #{e.message}" if print
+  shell_cmd(cmd, print: print)
+end
+
+def upload!(file, print: true)
+  puts "uploading: #{file}" if print
+  load_yaml(file, print)
 end
 
 # provides development-related utilities
