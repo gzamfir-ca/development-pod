52ba527bf4ce82578a96206ea8e81d6df6016fc5
gzamfir-ca <gzamfir_ca@icloud.com>
2025-11-26 07:50:09 -0500

    +boot dev pod

--- lib/development/pod/boot_pod.rb
+++ lib/development/pod/boot_pod.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
 module Development
-  # provides java-specific implementation
-  class JavaPod < Pod
+  # provides boot-specific implementation
+  class BootPod < Pod
     # class public methods
     def create
       return "#{self.class.name}:#{__method__} not allowed!" unless Development.operational?
 
-      tool = %w[gradle init --console verbose].tap { |t| append_options(t) }
-      res_ok = system!(tool)
+      tool1 = %w[curl https://start.spring.io/starter.tgz].tap { |t| append_options(t) }
+      tool2 = %w[tar -xzvf -]
+      res_ok = system!(tool1, tool2)
       "#{self.class.name}:#{__method__} #{res_ok ? "succeeded!" : "failed!"}"
     end
 
     def deploy
       return "#{self.class.name}:#{__method__} not allowed!" unless Development.operational?
 
-      tool = %w[gradle run --console verbose]
+      tool = %w[gradle bootRun --console verbose]
       res_ok = system!(tool)
       "#{self.class.name}:#{__method__} #{res_ok ? "succeeded!" : "failed!"}"
     end
