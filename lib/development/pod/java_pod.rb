d410c2a6ab24f1a4e8cbf5cfa21639a33ed7cc85
gzamfir-ca <gzamfir_ca@icloud.com>
2025-11-28 19:31:12 -0500

    +java dev pod

--- lib/development/pod/java_pod.rb
+++ lib/development/pod/java_pod.rb
@@ -3,12 +3,30 @@
 module Development
   # provides java-specific implementation
   class JavaPod < Pod
+    # class private methods
+    def fetch_data
+      tool = %w[gradle init --console verbose].tap { |t| append_options(t) }
+      system!(tool)
+    end
+
+    def patch_file
+      lines = File.readlines("settings.gradle")
+      lines.select do |line|
+        line.strip.start_with?("include", "rootProject")
+      end
+      File.open("settings.gradle", "w") { |f| f.puts lines }
+      true
+    rescue StandardError => e
+      puts "#{self.class.name}:#{__method__} p_file call failed: #{e.message}"
+      false
+    end
+
     # class public methods
     def create
       return "#{self.class.name}:#{__method__} not allowed!" unless Development.operational?
 
-      tool = %w[gradle init --console verbose].tap { |t| append_options(t) }
-      res_ok = system!(tool)
+      res_ok = fetch_data
+      res_ok = patch_file && res_ok
       "#{self.class.name}:#{__method__} #{res_ok ? "succeeded!" : "failed!"}"
     end
 
