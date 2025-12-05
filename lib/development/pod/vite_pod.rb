734eb016683c2fc4dcb66fbc7b69db86bdfe68e2
gzamfir-ca <gzamfir_ca@icloud.com>
2025-12-04 20:13:36 -0500

    +vite dev pod

--- lib/development/pod/vite_pod.rb
+++ lib/development/pod/vite_pod.rb
@@ -13,6 +13,17 @@ module Development
       [{ "template" => "react-ts" }, { "no-interactive" => nil }]
     end
 
+    def run_update
+      tool1 = %w[npm install]
+      tool2 = %w[npm run dev]
+      FileUtils.cd("app", verbose: true) do
+        system!(tool1, nil) && system!(tool2, nil)
+      end
+    rescue StandardError => e
+      puts "#{self.class.name}:#{__method__} runtime call failed: #{e.message}"
+      false
+    end
+
     def setup_core
       update_options && fetch_data
     end
@@ -32,6 +43,19 @@ module Development
       end
 
       res_ok = setup_core && Development.append_timestamp
+      msg = format(test_msg(res_ok), self.class.name, __method__)
+      puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
+      res_ok ? 0 : 1
+    end
+
+    def update
+      unless Development.operational?
+        msg = format(FAIL_MSG, self.class.name, __method__)
+        puts "#{FAIL_COLOR}#{msg}#{BACK_COLOR}"
+        return 1
+      end
+
+      res_ok = run_update
       msg = format(test_msg(res_ok), self.class.name, __method__)
       puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
       res_ok ? 0 : 1
