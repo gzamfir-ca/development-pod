045c96a7f90f26eba3e58ff0f51ad0b01f5ac002
gzamfir-ca <gzamfir_ca@icloud.com>
2025-12-04 19:22:36 -0500

    +vite dev pod

---
+++ lib/development/pod/vite_pod.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Development
+  # provides vite-specific implementation
+  class VitePod < Pod
+    # class private methods
+    def fetch_data
+      tool = %w[npm create vite@latest app --].tap { |t| append_options(t) }
+      system!(tool, nil)
+    end
+
+    def setup_core
+      fetch_data
+    end
+
+    # class public methods
+    def create
+      unless Development.operational?
+        msg = format(FAIL_MSG, self.class.name, __method__)
+        puts "#{FAIL_COLOR}#{msg}#{BACK_COLOR}"
+        return 1
+      end
+
+      res_ok = setup_core && Development.append_timestamp
+      msg = format(test_msg(res_ok), self.class.name, __method__)
+      puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
+      res_ok ? 0 : 1
+    end
+  end
+end
