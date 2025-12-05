7495e16da21c0c31d66ff0751a3bca7b0d1edc81
gzamfir-ca <gzamfir_ca@icloud.com>
2025-12-05 15:02:06 -0500

    +vite dev pod

--- lib/development/pod/vite_pod.rb
+++ lib/development/pod/vite_pod.rb
@@ -13,6 +13,17 @@ module Development
       [{ "template" => "react-ts" }, { "no-interactive" => nil }]
     end
 
+    def run_deploy
+      tool1 = %w[npm run build]
+      tool2 = %w[npm run preview]
+      FileUtils.cd("app", verbose: true) do
+        system!(tool1, nil) && system!(tool2, nil)
+      end
+    rescue StandardError => e
+      puts "#{self.class.name}:#{__method__} runtime call failed: #{e.message}"
+      false
+    end
+
     def run_update
       tool1 = %w[npm install]
       tool2 = %w[npm run dev]
@@ -43,6 +54,19 @@ module Development
       end
 
       res_ok = setup_core && Development.append_timestamp
+      msg = format(test_msg(res_ok), self.class.name, __method__)
+      puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
+      res_ok ? 0 : 1
+    end
+
+    def deploy
+      unless Development.operational?
+        msg = format(FAIL_MSG, self.class.name, __method__)
+        puts "#{FAIL_COLOR}#{msg}#{BACK_COLOR}"
+        return 1
+      end
+
+      res_ok = run_deploy
       msg = format(test_msg(res_ok), self.class.name, __method__)
       puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
       res_ok ? 0 : 1
