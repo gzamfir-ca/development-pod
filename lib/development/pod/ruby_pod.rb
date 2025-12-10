6822622015bd2412eaf27af913c32993d1ab517b
gzamfir-ca <gzamfir_ca@icloud.com>
2025-12-10 10:21:40 -0500

    +ruby dev pod

--- lib/development/pod/ruby_pod.rb
+++ lib/development/pod/ruby_pod.rb
@@ -5,35 +5,24 @@ module Development
   class RubyPod < Pod
     # class private methods
     def fetch_data?
-      app = Development.pod_name
-      tool = %W[bundle gem #{app}].tap { |t| append_options(t) }
+      tool = %W[bundle gem #{Development.pod_name}].tap { |t| append_options(t) }
       system?(tool, nil)
     end
 
     def patch_data?
-      app = Development.pod_name
       tool = %w[bundle exec rake rubocop:autocorrect_all]
-      FileUtils.cd(app, verbose: true) do
-        system?(tool, nil)
-      end
+      FileUtils.cd(Development.pod_name, verbose: true) { system?(tool, nil) }
     rescue StandardError => e
       puts "#{self.class.name}:#{__method__} runtime call failed: #{e.message}"
       false
     end
 
     def patch_file?
-      app = Development.pod_name
-      path = "./#{app}/spec/#{app}_spec.rb"
-      res_ok = Development.token_gsub?(path, "false", "true")
-      path = "./#{app}/.rubocop.yml"
-      original = "AllCops:"
-      replacement = "AllCops:\n  NewCops: enable\n  SuggestExtensions: false"
-      Development.token_gsub?(path, original, replacement) && res_ok
-      write_file?("./#{app}/Gemfile", ruby_data) && res_ok
-    end
-
-    def provide_options
-      [{ "exe" => nil }]
+      res_ok = Development.token_gsub?("./#{Development.pod_name}/spec/#{Development.pod_name}_spec.rb", "false",
+                                       "true")
+      Development.token_gsub?("./#{Development.pod_name}/.rubocop.yml", "AllCops:",
+                              "AllCops:\n  NewCops: enable\n  SuggestExtensions: false") && res_ok
+      write_file?("./#{Development.pod_name}/Gemfile", ruby_data) && res_ok
     end
 
     def ruby_data
@@ -49,6 +38,28 @@ module Development
       MULTILINE_CONTENT
     end
 
+    def run_deploy?
+      tool1 = %w[bundle exec rake build]
+      tool2 = %w[bundle exec rake install]
+      FileUtils.cd(Development.pod_name, verbose: true) do
+        system?(tool1, nil) && system?(tool2, nil)
+      end
+    rescue StandardError => e
+      puts "#{self.class.name}:#{__method__} runtime call failed: #{e.message}"
+      false
+    end
+
+    def run_update?
+      tool1 = %w[bundle install]
+      tool2 = %w[bundle exec rerun --clear --exit --verbose rspec]
+      FileUtils.cd(Development.pod_name, verbose: true) do
+        system?(tool1, nil) && system?(tool2, nil)
+      end
+    rescue StandardError => e
+      puts "#{self.class.name}:#{__method__} runtime call failed: #{e.message}"
+      false
+    end
+
     def setup_core?
       update_options? && fetch_data? && patch_file? && patch_data?
     end
@@ -56,7 +67,7 @@ module Development
     def update_options?
       return true if Development.entry?(Development::OPT_NAME)
 
-      Development.update_options?(provide_options)
+      Development.update_options?([{ "exe" => nil }])
     end
 
     def write_file?(file, data)
@@ -78,6 +89,32 @@ module Development
       end
 
       res_ok = setup_core? && Development.append_timestamp?
+      msg = format(test_msg(res_ok), self.class.name, __method__)
+      puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
+      res_ok ? 0 : 1
+    end
+
+    def deploy
+      unless Development.operational?
+        msg = format(FAIL_MSG, self.class.name, __method__)
+        puts "#{FAIL_COLOR}#{msg}#{BACK_COLOR}"
+        return 1
+      end
+
+      res_ok = run_deploy?
+      msg = format(test_msg(res_ok), self.class.name, __method__)
+      puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
+      res_ok ? 0 : 1
+    end
+
+    def update
+      unless Development.operational?
+        msg = format(FAIL_MSG, self.class.name, __method__)
+        puts "#{FAIL_COLOR}#{msg}#{BACK_COLOR}"
+        return 1
+      end
+
+      res_ok = run_update?
       msg = format(test_msg(res_ok), self.class.name, __method__)
       puts "#{test_color(res_ok)}#{msg}#{BACK_COLOR}"
       res_ok ? 0 : 1
