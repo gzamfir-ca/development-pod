6b98d2c7c18ccf054b7438f0390e526f72bb9792
gzamfir-ca <gzamfir_ca@icloud.com>
2025-12-16 16:08:04 -0500

    +ruby dev pod

--- lib/development/pod/ruby_pod.rb
+++ lib/development/pod/ruby_pod.rb
@@ -11,16 +11,30 @@ module Development
       run_tools?(".", [tool])
     end
 
+    def patch_gemspec?
+      gs_desc = "#{Development.pod_name} is a ruby gem"
+      gs_abre = "#{Development.pod_name} does something"
+      gs_file = "./#{Development.pod_name}/#{Development.pod_name}.gemspec"
+      gs_prop = "https://github.com/gzamfir-ca/#{Development.pod_name}"
+      gs_serv = "https://rubygems.org"
+      res_ok = token_gsub?(gs_file, "TODO: Write a short summary, because RubyGems requires one.", gs_abre)
+      res_ok = token_gsub?(gs_file, "TODO: Write a longer description or delete this line.", gs_desc) && res_ok
+      res_ok = token_gsub?(gs_file, "TODO: Put your gem's website or public repo URL here.", gs_prop) && res_ok
+      res_ok = token_gsub?(gs_file, "TODO: Put your gem's public repo URL here.", gs_prop) && res_ok
+      token_gsub?(gs_file, "TODO: Set to your gem server 'https://example.com'", gs_serv) && res_ok
+    end
+
     def patch_file?
+      gm_file = "./#{Development.pod_name}/Gemfile"
       sp_file = "./#{Development.pod_name}/spec/#{Development.pod_name}_spec.rb"
       rc_file = "./#{Development.pod_name}/.rubocop.yml"
       rc_prop = "AllCops:\n  NewCops: enable\n  SuggestExtensions: false"
       rc_skip = "AllCops:\n  Exclude:\n    - 'bin/*'"
-      gm_file = "./#{Development.pod_name}/Gemfile"
-      res_ok = token_gsub?(sp_file, "false", "true")
+      res_ok = update_file?(gm_file, "w", ruby_data)
+      res_ok = token_gsub?(sp_file, "false", "true") && res_ok
       res_ok = token_gsub?(rc_file, "AllCops:", rc_prop) && res_ok
       res_ok = token_gsub?(rc_file, "AllCops:", rc_skip) && res_ok
-      update_file?(gm_file, "w", ruby_data) && res_ok
+      patch_gemspec? && res_ok
     end
 
     def patch_path?
