80e1a0ca68ea1be5471cc46d16b933228cc73a19
gzamfir-ca <gzamfir_ca@icloud.com>
2026-02-19 13:41:05 -0500

    +java dev pod

--- lib/development/pod/java_pod.rb
+++ lib/development/pod/java_pod.rb
@@ -8,6 +8,52 @@ module Development
     # Class private methods
     include Hub
 
+    def conf_data
+      <<~CONF_DATA.chomp
+        configurations {
+            mockitoAgent {
+                transitive = false
+            }
+        }
+      CONF_DATA
+    end
+
+    def libs_data
+      <<~LIBS_DATA.chomp
+        implementation 'com.me.libs:libext:1.0.0'
+
+            mockitoAgent 'org.mockito:mockito-core:5.17.0'
+      LIBS_DATA
+    end
+
+    def repo_data
+      <<~REPO_DATA.chomp
+        mavenCentral()
+            mavenLocal()
+      REPO_DATA
+    end
+
+    def test_data
+      <<~TEST_DATA.chomp
+        testImplementation('org.junit.jupiter:junit-jupiter:6.0.3')
+            testImplementation('org.hamcrest:hamcrest:3.0')
+            testImplementation('org.mockito:mockito-core:5.21.0')
+      TEST_DATA
+    end
+
+    def unit_data
+      <<~UNIT_DATA.chomp
+        useJUnitPlatform()
+            jvmArgs(
+                "-javaagent:${configurations.mockitoAgent.singleFile}",
+                "-Xshare:off"
+            )
+            testLogging {
+                showStandardStreams = true
+            }
+      UNIT_DATA
+    end
+
     def fetch_data?
       tool = %w[gradle init --console verbose].tap { |t| append_options(t) }
       path = Pathname.new(".").expand_path
@@ -15,15 +61,8 @@ module Development
     end
 
     def patch_build?
-      substitutions = {
-        "testImplementation libs.junit.jupiter" => "testImplementation('org.junit.jupiter:junit-jupiter:6.0.3')\n\
-    testImplementation('org.hamcrest:hamcrest:3.0')",
-        "libs.guava" => "'com.me.libs:libext:1.0.0'",
-        "mavenCentral()" => "mavenCentral()\n    mavenLocal()",
-        "useJUnitPlatform()" => "useJUnitPlatform()\n    testLogging {\n        showStandardStreams = true\n    }"
-      }
       file = Pathname.new("app/build.gradle").expand_path
-      substitutions.all? { |search, replace| token_gsub?(file, search, replace) }
+      sub_map.all? { |search, replace| token_gsub?(file, search, replace) }
     end
 
     def patch_file?
@@ -67,6 +106,16 @@ module Development
       tool = %w[gradle test --continuous --console verbose]
       path = Pathname.new(Development.pod_name).expand_path
       run_tools?(path, [tool])
+    end
+
+    def sub_map
+      {
+        "dependencies {" => "#{conf_data}\n\ndependencies {",
+        "    implementation libs.guava" => "    #{libs_data}",
+        "    mavenCentral()" => "    #{repo_data}",
+        "    testImplementation libs.junit.jupiter" => "    #{test_data}",
+        "    useJUnitPlatform()" => "    #{unit_data}"
+      }
     end
   end
 end
